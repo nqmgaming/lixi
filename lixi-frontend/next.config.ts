80e12f8d26821b39fec034f10e0b895c9b8e290f
Nguyen Quang Minh (NQM) <nguyenquangminh391@gmail.com>
2026-02-16 22:25:27 +0700

    Merge lixi-frontend into monorepo

--- lixi-frontend/next.config.ts
+++ lixi-frontend/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3001/api/:path*',
+        destination: 'https://lixiapi.pwhs.app/api/:path*',
       },
     ];
   },
